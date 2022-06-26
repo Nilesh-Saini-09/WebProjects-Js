f7ca8fb9f4e5696428063913b3e6910949c3c82d
N <cubingniles@gmail.com>
2022-06-26 18:47:38 +0530

    Add Loader

--- QuoteGenerator/script.js
+++ QuoteGenerator/script.js
@@ -1,13 +1,27 @@
-const quoteContainer = document.getElementById('quote-generator');
+const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// show loading
+const loading = () => {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+const complete = () => {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 // Show New Quote
 const newQuote = () => {
+    loading();
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
     // populate author and quote in the ui
@@ -19,12 +33,14 @@ const newQuote = () => {
     } else {
         quoteText.classList.remove('long-quote');
     }
-
+    // Set Quote, Hide Loader
     quoteText.textContent = quote.text;
+    complete();
 }
 
 // Get Quotes From API
 const getQuotes = async () => {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
@@ -32,6 +48,9 @@ const getQuotes = async () => {
         newQuote();
     } catch (error) {
         // Catch Error Here
+        complete();
+        quoteContainer.textContent = 'Something Is Wrong. TRY AGAIN!';
+        console.log(err);
     }
 }
 
